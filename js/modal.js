07a2ba4f03258dcf291601cc9e5c8c838028b039
Paulo <paulofcoelho20@gmail.com>
2023-09-27 10:00:33 -0700

    teste script

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,16 @@
-// "use strict";
-// function typeWriter(el) {
-//   const textArray = el.innerHTML.split("");
-//   el.innerHTML = "";
+"use strict";
+function typeWriter(el) {
+  const textArray = el.innerHTML.split("");
+  el.innerHTML = "";
 
-//   textArray.forEach((letter, i) => {
-//     setTimeout(() => (el.innerHTML += letter), 95 * i);
-//   });
+  textArray.forEach((letter, i) => {
+    setTimeout(() => (el.innerHTML += letter), 95 * i);
+  });
 
-//   setInterval(() => typeWriter(el), 8000);
-// }
-// typeWriter(elementE2);
-// typeWriter(resumeText);
+  // setInterval(() => typeWriter(el), 8000);
+}
+typeWriter(elementE2);
+typeWriter(resumeText);
 
 //////////////////////////////////////////////////////////
 
